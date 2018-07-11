aaa70e50d41fa7d88c2f7f52468500ae72a50777
Tom King <design.tomking@gmail.com>
2018-07-11 16:39:58 -0600

    Add node files

---
+++ test/Node-test.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai')
+const { chai } = expect;
+const Node = require('../lib/Node')
+
+describe('NODE', () => {
+  let node;
+
+  beforeEach(() => {
+    node = new Node('pizza');
+  })
+
+  it('should exist', () => {
+    expect(node).to.exist;
+  })
+
+  it('should default next to null', () => {
+    expect(node.next).to.equal(null);
+  })
+
+  it('should take data and assign it to data prop', () => {
+    expect(node.data).to.equal('pizza');
+  })
+
+  it.skip('should be able to take in two next properties', () => {
+    
+  });
+
+  it.skip('should be able to take in multiple next properties', () => {
+
+  });
+})
