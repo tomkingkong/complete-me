2a291cc43fe65e915e288969f0c7812c60fbb7c5
Tom King <design.tomking@gmail.com>
2018-07-11 21:31:45 -0600

    Trie can insert words and keep count

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -3,14 +3,30 @@ const Node = require('../lib/Node')
 const Trie = require('../lib/Trie');
 
 describe('TRIE', () => {
-  let trie;
+  let prefixTrie;
 
   beforeEach(() => {
-    trie = new Trie();
+    prefixTrie = new Trie();
   });
 
   it('should start with zero words', () => {
-    expect(trie.wordCount).to.eq(0);
+    expect(prefixTrie.wordCount).to.eq(0);
+  });
+
+  it('should be able to add a word', () => {
+    prefixTrie.insert('hello');
+
+    expect(prefixTrie.hello).to.eq('hello');
+    expect(prefixTrie.wordCount).to.eq(1);
+  });
+
+  it('should keep track of words added', () => {
+    prefixTrie.insert('hello');
+    prefixTrie.insert('world');
+    prefixTrie.insert('goodbye');
+
+    expect(prefixTrie.wordCount).to.eq(3);
+    expect(prefixTrie.goodbye).to.eq('goodbye');    
   });
 
 });
